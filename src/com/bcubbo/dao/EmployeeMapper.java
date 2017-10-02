76910b9de960ca234d8ae18f7549e99ef662b18f
bowieu <fatalitywolf@foxmail.com>
2017-10-02 14:25:15 +0800

    配置项目整合配置文件

---
+++ src/com/bcubbo/dao/EmployeeMapper.java
@@ -0,0 +1,12 @@
+package com.bcubbo.dao;
+
+import java.util.List;
+
+import com.bcubbo.pojo.Employee;
+
+public interface EmployeeMapper {
+	
+	//获取员工列表
+	public List<Employee> getEmployeeList(Employee employee);
+
+}
---
+++ src/com/bcubbo/test/EmployeeDaoTest.java
@@ -0,0 +1,25 @@
+package com.bcubbo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeDaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		
+	}
+	
+	@Test
+	public void getEmployeeListTest() {
+		
+	}
+
+}
